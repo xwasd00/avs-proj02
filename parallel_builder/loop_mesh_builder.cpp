191df4fe3f0a741c439b90d7b3ddbbecefb62c89
xwasd00 <xsovam00@stud.fit.vutbr.cz>
2023-11-21 16:12:59 +0100

    pridani obrazku a pridani autora do hlavicek

--- parallel_builder/loop_mesh_builder.cpp
+++ parallel_builder/loop_mesh_builder.cpp
@@ -1,7 +1,7 @@
 /**
  * @file    loop_mesh_builder.cpp
  *
- * @author  FULL NAME <xlogin00@stud.fit.vutbr.cz>
+ * @author  Michal Sova <xsovam00@stud.fit.vutbr.cz>
  *
  * @brief   Parallel Marching Cubes implementation using OpenMP loops
  *
--- parallel_builder/tree_mesh_builder.cpp
+++ parallel_builder/tree_mesh_builder.cpp
@@ -1,7 +1,7 @@
 /**
  * @file    tree_mesh_builder.cpp
  *
- * @author  FULL NAME <xlogin00@stud.fit.vutbr.cz>
+ * @author  Michal Sova <xsovam00@stud.fit.vutbr.cz>
  *
  * @brief   Parallel Marching Cubes implementation using OpenMP tasks + octree early elimination
  *
